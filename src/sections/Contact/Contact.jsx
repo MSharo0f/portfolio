eb1018ef62cdc5052278a543de6ee31b221db64d
Simple Shaw <msharoof@protonmail.com>
2024-07-10 05:44:01 -0700

    added captcha

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,13 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import ReCAPTCHA from "react-google-recaptcha";
 import styles from "./ContactStyles.module.css";
 
 function Contact() {
+  const [recaptchaValue, setRecaptchaValue] = useState(null);
+
+  const handleRecaptchaChange = (value) => {
+    setRecaptchaValue(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!recaptchaValue) {
+      alert("Please complete the reCAPTCHA");
+      return;
+    }
+
+   
+    const formData = new FormData(event.target);
+
+   
+    formData.append('g-recaptcha-response', recaptchaValue);
+
+  
+    fetch(event.target.action, {
+      method: event.target.method,
+      body: formData,
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        alert("Thanks for your submission!");
+        event.target.reset();
+      } else {
+        alert("Failed to submit. Please try again.");
+      }
+    }).catch(error => {
+      alert("There was a problem submitting the form. Please try again.");
+    });
+  };
+
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action="https://formspree.io/f/meojqwgg" method="post">
+      <form 
+        action={import.meta.env.VITE_EMAIL}
+        method="post"
+        onSubmit={handleSubmit}
+      >
         <div className="formGroup">
-          <label htmlFor="message" hidden>
+          <label htmlFor="name" hidden>
             Name
           </label>
           <input
@@ -41,7 +84,13 @@ function Contact() {
             required
           ></textarea>
         </div>
-        <input className="hover btn" type="submit" value="Submit"/>
+        <div className="formGroup" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <ReCAPTCHA 
+            sitekey={import.meta.env.VITE_RECAPTCHA_SITE_KEY}
+            onChange={handleRecaptchaChange}
+          />
+        </div>
+        <input className="hover btn" type="submit" value="Submit" />
       </form>
     </section>
   );
